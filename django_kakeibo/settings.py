3e59e270aaf295ad03880a9e62fbdef3536b206f
koralle <mkhd7930@gmail.com>
2020-01-05 21:17:58 +0900

    DBをPostgreSQLに変更

--- django_kakeibo/settings.py
+++ django_kakeibo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_kakeibo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'koralle',
+        'USER': 'postgres',
+        'PASSWORD': 'koralle',
+        'HOST': 'localhost',
+        'POST': 5432
     }
 }
 
